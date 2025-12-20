6c858aee2f4b26c09b960e4793cc80d601e011c8
Noé Sánchez <noeszc@gmail.com>
2025-12-20 14:47:22 -0600

    feat(nvim): add ts-comments, todo-comments, and tmux-navigator

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,50 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out =
-    vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-require "noeszc.options"
-require "noeszc.keymaps"
-
+require("noeszc.options")
+require("noeszc.keymaps")
 
 require("lazy").setup({
-  spec = {
-{ import = "plugins" },
-  },
-  defaults = { lazy = false },
-  install = { colorscheme = { "tokyonight-moon" } },
+	spec = {
+		-- --- smart commenting based on treesitter
+		{ "folke/ts-comments.nvim", opts = {}, event = "BufReadPre" },
+
+		-- --- highlight and search for TODO, FIX, HACK, etc.
+		{
+			"folke/todo-comments.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			opts = {},
+			event = "BufReadPre",
+		},
+
+		-- --- seamless navigation between nvim splits and tmux panes
+		{
+			"christoomey/vim-tmux-navigator",
+			keys = {
+				-- silent = true ensures the command doesn't echo in the command line
+				{ "<C-H>", "<cmd><C-U>TmuxNavigateLeft<cr>", silent = true },
+				{ "<C-J>", "<cmd><C-U>TmuxNavigateDown<cr>", silent = true },
+				{ "<C-K>", "<cmd><C-U>TmuxNavigateUp<cr>", silent = true },
+				{ "<C-L>", "<cmd><C-U>TmuxNavigateRight<cr>", silent = true },
+			},
+		},
+		{ import = "plugins" },
+	},
+	defaults = { lazy = false },
+	install = { colorscheme = { "tokyonight-moon" } },
 })
