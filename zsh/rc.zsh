671eb2dfcacdc1a562183af516fa428906a4bd8b
Noé Sánchez <noeszc@gmail.com>
2025-12-27 22:25:48 -0600

    add fzf theme and commit mono font

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -36,9 +36,6 @@ fi
 if type "direnv" > /dev/null; then
     eval "$(direnv hook zsh)"
 fi
-
-
-
 autoload -U zmv
 autoload -U promptinit && promptinit
 autoload -U colors && colors
@@ -47,6 +44,12 @@ autoload -Uz compinit && compinit
 SOBOLE_THEME_MODE='dark'
 export VISUAL=nvim
 export EDITOR=nvim
+export FZF_DEFAULT_OPTS="
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
 
 export PATH="$PATH:/usr/local/sbin:$DOTFILES"
 
